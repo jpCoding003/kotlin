8029ab53009ca8b3e36fcd176b206aea640d9a9d
jpCoding003 <jpcoding003@gmail.com>
2025-04-17 11:16:41 +0530

    Function

---
+++ src/Functions.kt
@@ -0,0 +1,38 @@
+fun add( x:Int=0,  y:Int=0,  z:Int=0){
+    val add = x + y + z
+    println("Addition of $x , $y and $z  is : $add")
+}
+
+fun multi(x:Int=0,  y:Int=0,  z:Int=0){
+    val mul = x * y * z
+    println("Multiplication of $x , $y and $z  is : $mul")
+}
+
+fun div(x:Double=0.0,  y:Double=0.0,  z:Double=0.0){
+    val div:Double = (x / y) / z
+    println("Division of $x , $y and $z  is : $div")
+}
+
+fun sub(x:Int=0,  y:Int=0,  z:Int=0){
+    val sub = x - y - z
+    println("Subtraction of $x , $y and $z  is : $sub")
+}
+
+
+fun main(){
+
+    print("\nEnter Number X :")
+    val x = readln().toInt()
+
+    print("\nEnter Number Y :")
+    val y = readln().toInt()
+
+    print("\n Enter Number Z :")
+    val z = readln().toInt()
+
+    add(x,y,z)
+    sub(x,y,z)
+    multi(x,y,z)
+    div(x.toDouble(),y.toDouble(),z.toDouble())
+
+}
--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,21 @@
+
+fun add( x:Int ,y: Int) {
+
+        val a = x+y
+    println("We are in outer functio!!!")
+    println("Addition of two number ${x} & ${y}: $a")
+
+    }
+
 fun main() {
     println("Hello World!")
     val a =10
     val b = 15
     println(" Addition :${a+b}")
     println(" Division :${(a+b)/3 }")
+
+    add(123,500)
+
 
 //    a=10  variable declare with " Val " can't allow to change value
 //    initialize variable with var where the change of values in specific variable is needed else always use val
