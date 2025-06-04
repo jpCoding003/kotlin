062abc4c64ba1aaf8e39eb65a63c0a99797f07c5
jpCoding003 <jpcoding003@gmail.com>
2025-06-04 23:12:28 +0530

    //     1  //    212  //   32123  //  4321234  // 543212345  //  4321234  //   32123  //    212  //     1

--- src/Pattern2.kt
+++ src/Pattern2.kt
@@ -2,7 +2,7 @@ fun main(){
     val n =5
     for (i in 1..2*n){                               //  *
      //   var value = i>n ? (2*n)-i-1 : n                  //  * *
-        var value = if (i>n){(2*n)-i-1} else{i}              //  * * *
+        var value = if (i>n){(2*n)-i-1} else{i}            //  * * *
             for (j in 1..value) {                    //  * * * *         // Using Double Loop
                 print("*")                                 //  * * *
             }                                              //  * *             // here two loops are not combine to create this patter!!!!!!!
@@ -21,20 +21,41 @@ fun main(){
                 a++
             }
         }
+        println()
     }
     println()
 
      for (i in 1..n){
-         for (j in 1..(n-i)){          //     1
-             print(" ")                      //    212
-         }                                   //   32123
-         for (j in i downTo 1){        //  4321234
-             print(j)                        // 543212345
+         for (j in 1..(n-i)){              //     1
+             print("  ")                         //    212
+         }                                       //   32123
+         for (j in i downTo 1){            //  4321234
+             print("$j ")                        // 543212345
          }
          for (j in 2..i){
-             print(j)
+             print("$j ")
          }
          println()
      }
+
+    println("\n")
+
+
+    for (i in 1..2*n){
+
+        var c  = if(i > n ) {2 * n - i} else {i}
+        for (j in 1..(n-c)){              //     1
+            print("  ")                         //    212
+        }                                       //   32123
+        for (j in c downTo 1){            //  4321234
+            print("$j ")                        // 543212345
+        }
+        for (j in 2..c){
+            print("$j ")
+        }
+        println()
+    }
+
+
 
 }
